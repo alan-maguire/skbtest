908029228756d5343a7cb3496c2e5e46a2231d97
Alan Maguire <alan.maguire@oracle.com>
2018-11-28 18:29:24 +0100

    fix copyright
    Signed-off-by: Alan Maguire <alan.maguire@oracle.com>

--- kernel/skbtest.c
+++ kernel/skbtest.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
  *    Author: Alan Maguire <alan.maguire@oracle.com>
  *
  * This program is free software; you can redistribute it and/or modify
